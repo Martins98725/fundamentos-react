644d72a2952dc3947e6887a6b7d9f7a8278b1bce
Martins98725 <icaro25martins@gmail.com>
2025-04-20 19:22:33 -0300

    feat: adicionando componente de header e o css global

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import { Header } from "./components/header";
 import { Post } from "./post";
 
 export function App() {
   return (
     <>
-      <h1>hello world</h1>
-      <Post content="asbdhdbashdbhasdbashdbashdbashb" author="icaro"/> 
-      <Post content="dasdadsdaddsd" author="teste"/>
+      <Header />
+      <Post content="asbdhdbashdbhasdbashdbashdbashb" author="icaro" />
+      <Post content="dasdadsdaddsd" author="teste" />
     </>
   );
 }
-
